381239e7f9ef9acdfc810a6869dd10904bcff91c
fan9704 <cxz123499@gmail.com>
2022-07-25 00:36:21 +0800

    According MachineStorageUnittest Fix Machine_storageDTO Add NoArgsConstructor

--- src/main/java/com/bezkoder/spring/datajpa/dto/Machine_storageDTO.java
+++ src/main/java/com/bezkoder/spring/datajpa/dto/Machine_storageDTO.java
@@ -1,11 +1,13 @@
 package com.bezkoder.spring.datajpa.dto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Machine_storageDTO {
     private  long machine_id;
     private  long garbage_type;
