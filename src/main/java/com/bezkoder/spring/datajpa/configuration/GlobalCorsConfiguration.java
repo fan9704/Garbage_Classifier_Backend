8a112948b2a6b51b7e524130a6fbfa91c5d267df
fan9704 <cxz123499@gmail.com>
2022-08-04 00:09:36 +0800

    Optimized GlobalCorsConfiguration Comment and Import

--- src/main/java/com/bezkoder/spring/datajpa/configuration/GlobalCorsConfiguration.java
+++ src/main/java/com/bezkoder/spring/datajpa/configuration/GlobalCorsConfiguration.java
@@ -1,13 +1,8 @@
 package com.bezkoder.spring.datajpa.configuration;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -15,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class GlobalCorsConfiguration implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                //映射路徑
                 registry.addMapping("/**")
                         .allowedOrigins("*")
 //                        .allowCredentials(true)
