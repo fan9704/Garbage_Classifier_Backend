28765009ab351943b622f8a1be0b7be4460c13f1
fan9704 <cxz123499@gmail.com>
2022-08-07 00:21:41 +0800

    Create GraphQL BankTypeResolver

---
+++ src/main/java/com/bezkoder/spring/datajpa/GraphQL/resovler/BankTypeResolver.java
@@ -0,0 +1,9 @@
+package com.bezkoder.spring.datajpa.GraphQL.resovler;
+
+import com.bezkoder.spring.datajpa.model.Bank_type;
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BankTypeResolver implements GraphQLResolver<Bank_type> {
+}
