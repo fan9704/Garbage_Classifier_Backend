04d1658b5fe236432e40ecfdcc07eff9c80a55fd
fan9704 <cxz123499@gmail.com>
2022-08-14 00:02:27 +0800

    Complete RoleRepositoryTest Unittest

---
+++ src/test/java/com/bezkoder/spring/datajpa/repository/RoleRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.bezkoder.spring.datajpa.repository;
+
+import com.bezkoder.spring.datajpa.configuration.DaoConfig;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = DaoConfig.class)
+public class RoleRepositoryTest {
+}
