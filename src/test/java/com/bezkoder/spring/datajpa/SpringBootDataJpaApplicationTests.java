ffaa90a67c9ccedaab8dd0a27f72c7285a305ce0
fan9704 <cxz123499@gmail.com>
2022-09-01 00:05:23 +0800

    fix SpringBootDataJpaApplication.run.xml description

--- src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -1,11 +1,9 @@
 package com.bezkoder.spring.datajpa;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDataJpaApplicationTests {
 
 	@Test
